4a10f29fc78846ca06632944441118420c07e245
Thiago Felipe <thiago.felipe@tutanota.com>
2021-05-07 13:32:08 -0400

    Fazendo os inputs necessários para enviar a mensagem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
 import './App.css';
+import Inputs from './components/Inputs';
+
+const Main = styled.main`
+  background-color: #e5ddd5;
+  max-width: 600px;
+  width: 100%;
+  border: 1px solid #000000;
+  height: 100vh;
+  margin: 0 auto;
+`
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Main>
+      <Inputs enviarMensagem={ (mensagem) => console.log(mensagem) } />
+    </Main>
   );
 }
 
